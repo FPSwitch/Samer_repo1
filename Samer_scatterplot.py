edd05bad13199fb6e798e558ba9446f2d2f420f2
FPSwitch <sytech8920@protonmail.com>
2024-01-29 20:58:58 -0800

    added csv file and updated scatter plot

--- Samer_scatterplot.py
+++ Samer_scatterplot.py
@@ -1,3 +1,12 @@
-import matplotlib.pyplot as plt
+import pandas as pd           # to read csv files and plot the data
 import numpy as np
+import matplotlib.pyplot as plt
 
+
+data = pd.read_csv(weeks.csv)   #reads csv file that has info
+
+X = data.iloc[:, 0]    # reads the file variables found from FileTouches script
+Y = data.iloc[:,1]     # reads column of amount of weeks
+
+plt.scatter(X, Y, c=Y, s=100, cmap='blue')    # create scatter plot
+plt.show()   #prints plot
